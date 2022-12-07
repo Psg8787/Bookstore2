29fd8cc7f24893e0a818f58701402f6dfd0edecd
Pratik Gupta <guptapratik022@gmail.com>
2022-12-07 17:00:24 +0530

    new

---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+// function myinput(){
+//     let filter = document.getElementById("inpt").ariaValueMax.toUpperCase();
+//     let table = document.getElementById("table");
+//     let tr = table.document.getElementsByTagName("tr");
+//     for(i=0;i<tr.length;i++){
+//         let td = tr[i].getElementsByTagName("td")[0];
+//         if(td){
+//             let value = td.textContent || td.innerHTML;
+//             if(value.toUpperCase.indexOf(filter) > -1){
+//                 tr.[i].style.display = "";
+//             }else{
+//                 tr[i].style.display = "none";
+//             }
+//         }
+//     }
+// }
+// address book
+
+// window.onload function(){
+//     var quickAddbtn = document.getElementById("quickadd");
+//     var Addbtn = document.getElementById("add");
+//     var cancelbtn = document.getElementById("cancel");
+//     var quickaddformbtn = document.getElementsByClassName("quickaddform")[0];
+
+
+//     var fullname = document.getElementById("fullname");
+//     var phone = document.getElementById("phone");
+//     var address = document.getElementById("address");
+//     var city = document.getElementById("city");
+//     var email = document.getElementById("email");
+
+
+//     // address book display
+
+
+
+// }
